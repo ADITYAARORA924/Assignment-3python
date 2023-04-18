3eb6708c904f9fb8610a106df631c0f0aabb093e
ADITYAARORA924 <131006921+ADITYAARORA924@users.noreply.github.com>
2023-04-18 18:49:59 +0530

    Add files via upload

---
+++ Assignment Q2.py
@@ -0,0 +1,8 @@
+name="Aditya Arora"
+department="Civil"
+SID= str(22102063)
+CGPA= str(9.9)
+print("Hey,"+name+" here")
+print("My SID is "+ SID)
+print("I am from "+department+" department and my CGPA is "+CGPA)
+
